8657e4fb0c6b7ab07190344ffb45e59aa2722f7a
borhanshuvo <borhan015@gmail.com>
2021-06-27 23:06:56 +0600

    changes

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,8 +1,9 @@
 import React from 'react';
+
 import eventImg from '../../../images/event.png';
 
 const HeaderMain = () => {
-    const btnDesign = { fontSize: '20px', backgroundImage: 'linear-gradient(90deg, #19D3AF, #0FCFEA)', border: 'none', padding: '15px 30px', color: 'white', borderRadius: '10px' };
+    const btnDesign = { fontSize: '20px', backgroundImage: 'linear-gradient(90deg, #19D3AF, #0FCFEA)', border: 'none', padding: '15px 30px', color: 'white', borderRadius: '10px', textDecoration: 'none' };
     const textColor2 = { color: '#b4b5b5' };
     const headingColor = { color: '#3A4256' };
     const textColor = { color: '#3A3056' };
@@ -14,7 +15,7 @@ const HeaderMain = () => {
                         <div className="pt-5">
                             <h1 className="pb-5" style={headingColor}>WE CREATE <br /> YOU CELEBRATE</h1>
                             <p className="pb-5" style={textColor}>The process of planning and coordinating the event is usually referred to as event planning and which can include budgeting, scheduling, site selection, acquiring necessary permits, coordinating transportation and parking, arranging for speakers or entertainers, arranging decor, event security, catering, coordinating with third-party vendors, and emergency plans.</p>
-                            <button style={btnDesign}>Book Now</button>
+                            <a style={btnDesign} href="#service">Book Now</a>
                         </div>
                     </div>
                     <div className="col-md-6">
